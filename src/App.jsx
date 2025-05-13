034fec6f4793fdc3070fff744a0a186980a6a055
lmolsen <lmarieolsen@gmail.com>
2025-05-13 17:56:59 -0400

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.scss";
 import Contact from "./pages/Contact/Contact";
 import HomePage from "./pages/HomePage/HomePage";
+import Software from "./pages/SoftwareDev/Software";
+import Freelance from "./pages/FreelanceServices/Freelance";
+import Writing from "./pages/Writing/Writing";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<Contact/>}/>
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/software-dev" element={<Software />} />
+        <Route path="/freelance-services" element={<Freelance />} />
+        <Route path="/writing" element={<Writing />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,101 @@
 import "./Header.scss";
+import home from "./../../assets/icons/home.png";
+import options from "./../../assets/icons/options.png";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+    }
 
   return (
-    <header className={`header ${location.pathname === "/contact" ? "space": ""}`}>
-      <div className="header__container">
-        <Link className="header__link" to="/">
-          Home
-        </Link>
+    <>
+      <header
+        className={`header ${
+          location.pathname != "/software-dev" ? "space" : ""
+        }`}
+      >
+        <div className="header__container">
+          <Link className="header__link header__link--icon" to="/">
+            <img src={home} alt="Home icon" className="header__icon" />
+          </Link>
+          <div className="header__block">
+            <div
+              className="header__dropdown"
+              onMouseEnter={toggleDropdown}
+              onMouseLeave={toggleDropdown}
+            >
+              <button className="header__link header__dropdown-button tablet">
+                Showcase
+              </button>
+              {isDropdownOpen && (
+                <div className="header__dropdown-menu">
+                  <Link className="header__dropdown-item" to="/software-dev">
+                    Software Development
+                  </Link>
+                  <Link className="header__dropdown-item" to="/writing">
+                    Writing & Poetry
+                  </Link>
+                  <Link
+                    className="header__dropdown-item"
+                    to="/freelance-services"
+                  >
+                    Freelance Services
+                  </Link>
+                </div>
+              )}
+            </div>
+            <Link className="header__link tablet" to="/contact">
+              Contact
+            </Link>
+          </div>
+          <button
+            className="header__link header__dropdown-button header__link--icon mobile"
+            onClick={toggleModal}
+          >
+            <img src={options} alt="Options icon" className="header__icon" />
+          </button>
+        </div>
+      </header>
 
-        <Link className="header__link" to="/contact">
-          Contact
-        </Link>
-      </div>
-    </header>
+
+        <div className={`modal ${isModalOpen ? "modal--open" : ""}`}>
+          <button className="modal__close" onClick={toggleModal}>
+            ✕
+          </button>
+          <nav className="modal__menu">
+            <Link
+              className="modal__link"
+              to="/software-dev"
+              onClick={toggleModal}
+            >
+              Software Development
+            </Link>
+            <Link className="modal__link" to="/writing" onClick={toggleModal}>
+              Writing & Poetry
+            </Link>
+            <Link
+              className="modal__link"
+              to="/freelance-services"
+              onClick={toggleModal}
+            >
+              Freelance Services
+            </Link>
+            <Link className="modal__link" to="/contact" onClick={toggleModal}>
+              Contact
+            </Link>
+          </nav>
+        </div>
+    
+    </>
   );
 }
