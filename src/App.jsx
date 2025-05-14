de2b09b7ed87b45a9e44075126ea799464502ce7
lmolsen <lmarieolsen@gmail.com>
2025-05-13 21:14:06 -0400

    adjust preloading strat

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,24 @@ import Freelance from "./pages/FreelanceServices/Freelance";
 import Writing from "./pages/Writing/Writing";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// preload images
+import heron from "./assets/images/heron.png";
+import quiet from "./assets/images/quiet.png";
+import antifa from "./assets/images/antifa.png";
+import io from "./assets/images/io.png";
+import pithy from "./assets/images/pithy.png";
+import emergence from "./assets/images/emergence.png";
+
 function App() {
+    useEffect(() => {
+      const images = [heron, quiet, antifa, io, pithy, emergence];
+
+      images.forEach((image) => {
+        const img = new Image();
+        img.src = image;
+      });
+    }, []);
+
   return (
     <Router>
       <Routes>
